cfcf555e21f270adb6edd0ad246e1907b5978b36
smlfin <senioravp71@gmail.com>
2026-01-17 14:31:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     // --- START: TWO-TIERED FRONT-END PASSWORD PROTECTION ---
-    const ACCESS_PASSWORD_FULL = "1"; // Full access password
-    const ACCESS_PASSWORD_LIMITED = "123";  // Limited access password
+    const ACCESS_PASSWORD_FULL = "VFL123"; // Full access password
+    const ACCESS_PASSWORD_LIMITED = "A";  // Limited access password
     let currentAccessLevel = null; // To store 'full' or 'limited'
     const accessDeniedOverlay = document.getElementById('accessDeniedOverlay');
     const dashboardContent = document.getElementById('dashboardContent');
